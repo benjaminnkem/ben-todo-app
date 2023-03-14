448c7d818f5d3779f10be9e6cf52818d27114a4d
Benjamin Nkem <benjaminnkemfrancis@gmail.com>
2023-03-14 02:02:27 +0100

    Finished working on the DELETE and show details feature

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import Navbar from "./components/Navbar";
+import NotFound from "./components/NotFound";
 import Todo from "./components/Todo";
+import TodoDetails from "./components/TodoDetails";
 
 function App() {
   return (
-    <div className="App">
-      <header>
-        <Navbar />
-      </header>
-      <main>
-        <div id="todo">
-          <Todo />
-        </div>
-      </main>
-    </div>
+    <Router>
+      <div className="App">
+        <header>
+          <Navbar />
+        </header>
+
+        <main>
+          <div id="main___content">
+            <Routes>
+              <Route exact path="/" element={<Todo />}></Route>
+              <Route path="/todos/:id" element={<TodoDetails />}></Route>
+              <Route path="*" element={<NotFound />}></Route>
+            </Routes>
+          </div>
+        </main>
+      </div>
+    </Router>
   );
 }
 
